1963e94efaf2ff8ca0dae03189edbee8145a7f31
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-12-18 11:02:33 +0100

    test

---
+++ src/main/java/com/example/cicdsonarjenkins/User.java
@@ -0,0 +1,32 @@
+package com.example.cicdsonarjenkins;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @SuppressWarnings("unused")
+    private String username;
+
+    @SuppressWarnings("unused")
+    private String email;
+
+
+    @SuppressWarnings("unused")
+    private String password;
+}
+
